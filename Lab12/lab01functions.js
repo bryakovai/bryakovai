6abeb7647727ad250e80dba785102d28999e2369
bryakovai <ant.bryakov@mail.ru>
2017-10-05 20:34:34 +0600

    Выполнял Lab12, задания 2354, 5164.

---
+++ Lab12/lab01functions.js
@@ -0,0 +1,15 @@
+﻿function task1860(str){
+	return "Мы стремимся к " + str;
+}
+
+function task4764(str){
+	return  str + " нас не остановят!";
+}
+
+var task2429 = function(str){
+	return " Программирование это " + str + "!"
+}
+
+var task7472 = function(str1, str2){
+	return str2+ " " + str1;
+}
---
+++ Lab12/lab02functions.js
@@ -0,0 +1,3 @@
+function task4411(x){
+	return parseFloat(x) + 7;
+}
